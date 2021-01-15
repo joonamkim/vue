f8fe56e76dfcd31fd58a2c3c5df5e165ae966405
joo-namkim <jnk8843@gmail.com>
2021-01-15 20:53:10 +0900

    first commit

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,105 @@
+<template>
+    <div id="employee-form">
+        <form @submit.prevent="handleSubmit">
+            <label>Fund Code</label>
+            <input
+                    type="text"
+                    :class="{ 'has-error': submitting && invalidName }"
+                    v-model="employee.name"
+                    @focus="clearStatus"
+                    @keypress="clearStatus"
+            />
+            <label>Currency</label>
+            <input
+                    type="text"
+                    :class="{ 'has-error': submitting && invalidEmail }"
+                    v-model="employee.email"
+                    @focus="clearStatus"
+            />
+            <label>Quantity</label>
+            <input
+                    type="text"
+                    :class="{ 'has-error': submitting && invalidEmail }"
+                    v-model="employee.id"
+                    @focus="clearStatus"
+            />
+
+
+            <p v-if="error && submitting" class="error-message">
+                ❗Please fill out all required fields
+            </p>
+            <p v-if="success" class="success-message">✅ New fund record successfully added</p>
+            <button>Add new Fund Record</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'employee-form',
+        data() {
+            return {
+                submitting: false,
+                error: false,
+                success: false,
+                employee: {
+                    name: '',
+                    email: '',
+                    id:'',
+                },
+            }
+        },
+        methods: {
+            handleSubmit() {
+                this.submitting = true
+                this.clearStatus()
+
+                if (this.invalidName || this.invalidEmail) {
+                    this.error = true
+                    return
+                }
+
+                this.$emit('add:employee', this.employee)
+                this.employee = {
+                    name: '',
+                    email: '',
+                    quantity: ''
+                }
+                this.error = false
+                this.success = true
+                this.submitting = false
+            },
+            clearStatus() {
+                this.success = false
+                this.error = false
+            }
+        },
+        computed: {
+            invalidName() {
+                return this.employee.name === ''
+            },
+
+            invalidEmail() {
+                return this.employee.email === ''
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    form {
+        margin-bottom: 2rem;
+    }
+
+    [class*='-message'] {
+        font-weight: 500;
+    }
+
+    .error-message {
+        color: #d33c40;
+    }
+
+    .success-message {
+        color: #32a95d;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import VueMaterial from 'vue-material'
+// import 'vue-material/dist/vue-material.min.css'
+// import 'vue-material/dist/theme/default.css'
+// App.use(VueMaterial)
+
+createApp(App).mount('#app')
